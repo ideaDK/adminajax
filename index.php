617316d256d36ecb74432e5b726d9bdf4a77919b
ideaDK <131539862+ideaDK@users.noreply.github.com>
2023-05-07 23:05:22 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,61 @@
+<?php 
+/**
+ **** AppzStory Admin Ajax ****
+ * 
+ * @link https://appzstory.dev
+ * @author Yothin Sapsamran (Jame AppzStory Studio)
+ */
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>AppzStory Admin</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- Favicons -->
+  <link rel="apple-touch-icon" sizes="180x180" href="admin/assets/img/favicons/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="admin/assets/img/favicons/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="admin/assets/img/favicons/favicon-16x16.png">
+  <link rel="manifest" href="admin/assets/img/favicons/site.webmanifest">
+  <link rel="mask-icon" href="admin/assets/img/favicons/safari-pinned-tab.svg" color="#5bbad5">
+  <link rel="shortcut icon" href="admin/assets/img/favicons/favicon.ico">
+  <meta name="msapplication-TileColor" content="#da532c">
+  <meta name="msapplication-config" content="admin/assets/img/favicons/browserconfig.xml">
+  <meta name="theme-color" content="#ffffff">
+
+  <!-- stylesheet -->
+  <link href="https://fonts.googleapis.com/css?family=Mali" rel="stylesheet">
+  <link rel="stylesheet" href="admin/assets/css/adminlte.min.css">
+  <link rel="stylesheet" href="admin/assets/css/style.css">
+  <link rel="stylesheet" href="admin/plugins/toastr/toastr.min.css">
+
+  <link rel="stylesheet" href="admin/assets/css/style.css">
+</head>
+<body>
+<div class="bg"></div>
+<div class="container h-100">
+  <div class="row justify-content-center align-items-center h-100">
+    <div class="col-md-6">
+      <div class="card shadow p-5">
+        <div class="card-header"> 
+          <h3 class="text-center font-weight-bold"> Admin Ajax Template </h3>
+        </div>
+        <div class="card-body py-5">
+            <a class="btn btn-primary btn-block text-white" href="admin/login.php"> เข้าสู่หน้าผู้ดูแลระบบ </a>
+        </div>
+        <footer class="text-secondary text-center">
+            สอนเขียนเว็บไซต์ด้วยตัวเอง <span class="text-pink">♥️</span>
+            <a class="border-primary" href="https://appzstory.dev" target="_blank">AppzStory Studio</a>
+        </footer>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- SCRIPTS -->
+<script src="admin/plugins/jquery/jquery.min.js"></script>
+<script src="admin/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="admin/plugins/toastr/toastr.min.js"></script>
+
+</body>
+</html>
